ee1381851d6c52ebf4690e782f62e216739ca221
Dmitry <usefullforit@gmail.com>
2025-03-20 11:37:02 +0300

    Fix tests

--- tests/GeneratorDifferenceTest.php
+++ tests/GeneratorDifferenceTest.php
@@ -11,7 +11,7 @@ class GeneratorDifferenceTest extends TestCase
     {
         $filePath1 = __DIR__ . "/fixtures/json/file1.json";
         $filePath2 = __DIR__ . "/fixtures/json/file2.json";
-        $expected = '{"- follow":false,"  host":"hexlet.io","- proxy":"123.234.53.22","- timeout":50,"+ timeout":20,"+ verbose":true}';
+        $expected = "- follow: false\n  host: hexlet.io\n- proxy: 123.234.53.22\n- timeout: 50\n+ timeout: 20\n+ verbose: true";
         $actual = genDiff($filePath1, $filePath2);
 
         $this->assertEquals($expected, $actual);
@@ -21,7 +21,7 @@ class GeneratorDifferenceTest extends TestCase
     {
         $filePath1 = __DIR__ . "/fixtures/yaml/file1.yaml";
         $filePath2 = __DIR__ . "/fixtures/yaml/file2.yaml";
-        $expected = '{"+ eeee":false,"- setting1":"Value 1","+ setting1":"asd","- setting2":200,"- setting3":true,"  setting6":"tree"}';
+        $expected = "+ eeee: false\n- setting1: Value 1\n+ setting1: asd\n- setting2: 200\n- setting3: true\n  setting6: tree";
         $actual = genDiff($filePath1, $filePath2);
 
         $this->assertEquals($expected, $actual);
