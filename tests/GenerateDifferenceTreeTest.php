c271283e07fd92b4ba5b8f0535a7252de04cebc3
Dmitry <usefullforit@gmail.com>
2025-03-26 20:23:22 +0300

    strict type testing

--- tests/GenerateDifferenceTreeTest.php
+++ tests/GenerateDifferenceTreeTest.php
@@ -20,7 +20,7 @@ class GenerateDifferenceTreeTest extends TestCase
         $expected = "{\n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}";
         $actual = genDiff($this->filePath1, $this->filePath2);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertSame($expected, $actual);
     }
 
     public function testPlainYaml(): void
@@ -28,6 +28,6 @@ class GenerateDifferenceTreeTest extends TestCase
         $expected = "{\n  + eeee: false\n  - setting1: Value 1\n  + setting1: asd\n  - setting2: 200\n  - setting3: true\n    setting6: tree\n}";
         $actual = genDiff($this->filePath3, $this->filePath4);
 
-        $this->assertEquals($expected, $actual);
+        $this->assertSame($expected, $actual);
     }
 }
